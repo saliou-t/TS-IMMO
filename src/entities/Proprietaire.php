7769bc4abdb1bf831d3d7a4d7ea677fe3f2c0d3d
saliou-t <zalltalla46@gmail.com>
2022-03-31 15:16:29 +0000

    installation de doctrine et creation de la classe Proprietaire

---
+++ src/entities/Proprietaire.php
@@ -0,0 +1,94 @@
+<?php
+// src/Product.php
+class Proprietaire
+{
+   /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    protected $id;
+
+    /**
+     * @var string
+     */
+    protected $nom;
+
+     /**
+     * @var string
+     */
+    protected $prenom;
+
+     /**
+     * @var string
+     */
+    protected $adresse;
+
+     /**
+     * @var string
+     */
+    protected $telephone;
+
+    /**
+     * @var string
+    */
+    protected $CNI;
+
+
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+    }
+
+    public function getAdresse()
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse($adresse)
+    {
+        $this->adresse = $adresse;
+    }
+
+    public function getTelephone()
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone($telephone)
+    {
+        $this->telephone = $telephone;
+    }
+
+    public function getCNI()
+    {
+        return $this->CNI;
+    }
+
+    public function setCNI($cni)
+    {
+        $this->CNI = $cni;
+    }
+
+}
